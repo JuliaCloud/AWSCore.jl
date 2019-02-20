d50934a5d3276e04b2748b97eea2d3fe119d9957
morris25 <sam.morrison@invenia.ca>
2019-02-20 11:49:20 -0600

    Fixes typo and fleshes out renewal tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -72,7 +72,7 @@ end
         creds = AWSCredentials("access_key_id" ,"secret_key")
         @test creds.token == ""
         @test creds.user_arn == ""
-        @test creds.account_number ===""
+        @test creds.account_number == ""
         @test creds.expiry == typemax(DateTime)
         @test creds.renew == nothing
     end
@@ -95,25 +95,60 @@ end
         @test creds.secret_key == "secret_key"
 
         # Creds should take on value of a returned AWSCredentials except renew function
-        id = "NEW_ID"
-        key = "NEW_KEY"
+        function gen_credentials()
+            i = 0
+            () -> (i += 1; AWSCredentials("NEW_ID_$i", "NEW_KEY_$i"))
+        end
+
         creds = AWSCredentials(
             "access_key_id",
             "secret_key",
-            renew = () -> AWSCredentials(id, key, renew = nothing)
+            renew = gen_credentials(),
+            expiry = now(UTC),
         )
+
+        @test creds.renew !== nothing
+        renewed = creds.renew()
+
         @test creds.access_key_id == "access_key_id"
         @test creds.secret_key == "secret_key"
-        @test creds.renew !== nothing
-        @test creds.renew().renew === nothing
+        @test creds.expiry <= now(UTC)
+        @test AWSCore.will_expire(creds)
+
+        @test renewed.access_key_id === "NEW_ID_1"
+        @test renewed.secret_key == "NEW_KEY_1"
+        @test renewed.renew === nothing
+        @test renewed.expiry == typemax(DateTime)
+        @test !AWSCore.will_expire(renewed)
         renew = creds.renew
 
-        newcreds = check_credentials(creds, force_refresh = true)
+        # Check renewal on time out
+        newcreds = check_credentials(creds, force_refresh = false)
         @test creds === newcreds
-        @test creds.access_key_id == id
-        @test creds.secret_key == key
+        @test creds.access_key_id == "NEW_ID_2"
+        @test creds.secret_key == "NEW_KEY_2"
         @test creds.renew !== nothing
         @test creds.renew === renew
+        @test creds.expiry == typemax(DateTime)
+        @test !AWSCore.will_expire(creds)
+
+        # Check renewal doesn't happen if not forced or timed out
+        newcreds = check_credentials(creds, force_refresh = false)
+        @test creds === newcreds
+        @test creds.access_key_id == "NEW_ID_2"
+        @test creds.secret_key == "NEW_KEY_2"
+        @test creds.renew !== nothing
+        @test creds.renew === renew
+        @test creds.expiry == typemax(DateTime)
+
+        # Check forced renewal works
+        newcreds = check_credentials(creds, force_refresh = true)
+        @test creds === newcreds
+        @test creds.access_key_id == "NEW_ID_3"
+        @test creds.secret_key == "NEW_KEY_3"
+        @test creds.renew !== nothing
+        @test creds.renew === renew
+        @test creds.expiry == typemax(DateTime)
     end
 
     mktemp() do config_file, config_io
