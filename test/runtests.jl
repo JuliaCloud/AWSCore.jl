2c4e2e84755c655c61281535aa6ff7d41a1fb8da
twinGu <martin.gutwin@uni-jena.de>
2019-04-25 15:10:42 +0200

    fixed indentation

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,24 +23,24 @@ aws = aws_config()
 
 @testset "NoAuth" begin
     pub_request1 = Dict{Symbol, Any}(
-    :service => "s3",
-    :headers => Dict{String, String}("Range" => "bytes=0-0"),
-    :content => "",
-    :resource => "/invenia-static-website-content/invenia_ca/index.html",
-    :url => "https://s3.us-east-1.amazonaws.com/invenia-static-website-content/invenia_ca/index.html",
-    :verb => "GET",
-    :region => "us-east-1",
-    :creds => nothing,
+        :service => "s3",
+        :headers => Dict{String, String}("Range" => "bytes=0-0"),
+        :content => "",
+        :resource => "/invenia-static-website-content/invenia_ca/index.html",
+        :url => "https://s3.us-east-1.amazonaws.com/invenia-static-website-content/invenia_ca/index.html",
+        :verb => "GET",
+        :region => "us-east-1",
+        :creds => nothing,
     )
     pub_request2 = Dict{Symbol, Any}(
-    :service => "s3",
-    :headers => Dict{String, String}("Range" => "bytes=0-0"),
-    :content => "",
-    :resource => "ryft-public-sample-data/AWS-x86-AMI-queries.json",
-    :url => "https://s3.amazonaws.com/ryft-public-sample-data/AWS-x86-AMI-queries.json",
-    :verb => "GET",
-    :region => "us-east-1",
-    :creds => nothing,
+        :service => "s3",
+        :headers => Dict{String, String}("Range" => "bytes=0-0"),
+        :content => "",
+        :resource => "ryft-public-sample-data/AWS-x86-AMI-queries.json",
+        :url => "https://s3.amazonaws.com/ryft-public-sample-data/AWS-x86-AMI-queries.json",
+        :verb => "GET",
+        :region => "us-east-1",
+        :creds => nothing,
     )
     response = nothing
     try
