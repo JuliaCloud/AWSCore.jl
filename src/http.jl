ccd12b24e86f0de48feb5afa77720175ae6a7176
Richard Fliam <Richard_Fliam@cable.comcast.com>
2019-05-16 22:56:04 -0600

    Allow Passing Through HTTP Connection Settings
    aws_config can now take a `:http_connection_settings` key with pairs
    like :connection_limit=>100 to modify the http connection settings.

--- src/http.jl
+++ src/http.jl
@@ -26,6 +26,12 @@ function http_request(request::AWSRequest)
             push!(options, (:response_stream, io))
         end
 
+        if haskey(request, :http_connection_settings)
+            for v in request[:http_connection_settings]
+                push!(options, v)
+            end
+        end
+
         verbose = debug_level - 1
 
         http_stack = HTTP.stack(redirect=false,
