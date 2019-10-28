7e2320ed01634516b43979e57bb102f30eb1296a
rofinn <rory.finnegan@gmail.com>
2019-10-28 15:53:11 -0500

    Fix exported copy! deprecation which conflicts with the base copy!

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,2 +1,3 @@
 # https://github.com/JuliaCloud/AWSCore.jl/commit/b719614b812fbec385833e607a2f8772d6009b59
-Base.@deprecate copy!(dest::AWSCredentials, src::AWSCredentials) copyto!(dest, src)
+import Base: copy!
+Base.@deprecate copy!(dest::AWSCredentials, src::AWSCredentials) copyto!(dest, src) false
